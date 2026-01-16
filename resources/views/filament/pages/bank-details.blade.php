1b949ddb50ab8f79ec1ea2511d9fcc4384b313ff
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-01-16 09:45:10 -0800

    fix filament issues

--- resources/views/filament/pages/bank-details.blade.php
+++ resources/views/filament/pages/bank-details.blade.php
@@ -3,7 +3,11 @@
         <x-filament-panels::form wire:submit="save">
             {{ $this->form }}
 
-            <x-filament-panels::form.actions :actions="$this->getFormActions()" />
+            <div class="mt-6 flex items-center gap-3">
+                <x-filament::button type="submit">
+                    Save Details
+                </x-filament::button>
+            </div>
         </x-filament-panels::form>
     </div>
 </x-filament-panels::page>
