42264f6bd4870130bcdebfb20eade7b3a16072bc
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-01-16 09:48:07 -0800

    fix bank details

--- resources/views/filament/pages/bank-details.blade.php
+++ resources/views/filament/pages/bank-details.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     <div class="max-w-3xl">
-        <x-filament-panels::form wire:submit="save">
+        <form wire:submit="save">
             {{ $this->form }}
 
             <div class="mt-6 flex items-center gap-3">
@@ -8,6 +8,6 @@
                     Save Details
                 </x-filament::button>
             </div>
-        </x-filament-panels::form>
+        </form>
     </div>
 </x-filament-panels::page>
