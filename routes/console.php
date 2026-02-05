450b323e34229e92e9f92d2240249260bd85ddcf
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-02-05 04:06:45 -0800

    fix migration issue

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\SmartTenantsMigrate;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->purpose('Display an inspiring quote')->hourly();
+
+// Explicitly register SmartTenantsMigrate to ensure availability
+Artisan::starting(function ($artisan) {
+    $artisan->resolve(SmartTenantsMigrate::class);
+});
 
 use Illuminate\Support\Facades\Schedule;
 
